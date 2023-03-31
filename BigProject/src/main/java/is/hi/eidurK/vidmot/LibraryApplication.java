8679900a047cef6388905cdee7f15fa18226fc1b
janusbirgisson <janusbirgisson@gmail.com>
2023-03-31 10:00:29 +0000

    komumst svona langt á fös

--- BigProject/src/main/java/is/hi/eidurK/vidmot/LibraryApplication.java
+++ BigProject/src/main/java/is/hi/eidurK/vidmot/LibraryApplication.java
@@ -1,30 +1,11 @@
 package is.hi.eidurK.vidmot;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.stage.Stage;
-import java.io.IOException;
-import java.util.HashMap;
+public class LibraryApplication {
 
-import is.hi.eidurK.vidmot.LibraryApplication;
-
-public class LibraryApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("bouncing-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        BouncingController b = fxmlLoader.getController();
-        b.orvatakkar(fxmlLoader.getController(), scene);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
     public static void main(String[] args) {
-        launch();
-    }
-
 
     }
+}
+
+
+
